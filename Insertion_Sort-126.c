5bd872f0b15ddf92ddf951a6520ec3456a3ee847
MD Rasel Mamun <122204016+RaselmamunSD@users.noreply.github.com>
2024-01-25 00:49:46 +0600

    Add files via upload

---
+++ Insertion_Sort-126.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort(int array[], int size)
+{
+    for (int step = 0; step < size - 1; step++)
+    {
+        int min_idx = step;
+        for (int i = step + 1; i < size; i++)
+        {
+            if (array[i] < array[min_idx])
+                min_idx = i;
+        }
+        swap(&array[min_idx], &array[step]);
+    }
+}
+
+int main()
+{
+    int data[] = {20, 12, 10, 15, 2};
+    int size = sizeof(data) / sizeof(data[0]);
+    insertionSort(data, size);
+    printf("Sorted array in ascending order:\n");
+    printArray(data, size);
+}
